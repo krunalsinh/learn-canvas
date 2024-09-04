cdb266d796c681c3289e40c706fe5cc0e59f8bbf
Krunalsinh Vaghela <krunalsinh.vaghela@corpnet.co.in>
2024-09-04 18:04:00 +0530

    changes

--- 08 spawn elements and remove/page.js
+++ 08 spawn elements and remove/page.js
@@ -10,26 +10,15 @@ const colors = ['#e5989b', '#2a9d8f', '#e9c46a', '#f4a261', '#e76f51'];
 const mouse = { x: 100 / 2, y: 100, radius: 120 };
 
 
-let ball = new circle(ctx, 10, 10, 20, 'pink');
-let latestCord ;
-let dist;
+const arr = []
+const ball = new circle(ctx, 10, 10, 20, 'pink');
 
-let speed = 5;
 animationFunc();
-function animationFunc() {
+function animationFunc(time) {
+    // console.log(time);
+    
     fillRect(ctx, 0, 0, innerWidth, innerHeight, '#000');
     
-    latestCord = moveTo(ball.x, ball.y , mouse.x, mouse.y, speed);
-    dist = getDistance(ball.x, ball.y , mouse.x, mouse.y);
-    
-    ball.x += latestCord.x;
-    ball.y += latestCord.y;
-    if(dist < 5){
-        ball.color = 'transparent';
-    }else{
-        ball.color = 'red';
-    }
-    ball.draw();
 
     requestAnimationFrame(animationFunc);
 }
