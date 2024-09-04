7531d21c5c5963d817f044b7a37d269ca2e858d8
Krunalsinh Vaghela <krunalsinh.vaghela@corpnet.co.in>
2024-09-04 17:45:14 +0530

    changes

--- move to specific target/page.js
+++ move to specific target/page.js
@@ -7,14 +7,29 @@ canvas.height = innerHeight;
 canvas.width = innerWidth;
 
 const colors = ['#e5989b', '#2a9d8f', '#e9c46a', '#f4a261', '#e76f51'];
-const mouse = { x: innerWidth / 2, y: innerHeight / 2 , radius : 120};
-
-// animationFunc();
+const mouse = { x: 100 / 2, y: 100, radius: 120 };
 
 
+let ball = new circle(ctx, 10, 10, 20, 'pink');
+let latestCord ;
+let dist;
+
+let speed = 5;
 animationFunc();
 function animationFunc() {
     fillRect(ctx, 0, 0, innerWidth, innerHeight, '#000');
+    
+    latestCord = moveTo(ball.x, ball.y , mouse.x, mouse.y, speed);
+    dist = getDistance(ball.x, ball.y , mouse.x, mouse.y);
+    
+    ball.x += latestCord.x;
+    ball.y += latestCord.y;
+    if(dist < 5){
+        ball.color = 'transparent';
+    }else{
+        ball.color = 'red';
+    }
+    ball.draw();
 
     requestAnimationFrame(animationFunc);
 }
@@ -25,12 +40,13 @@ function getDistance(x1, y1, x2, y2) {
     return Math.sqrt(Math.pow(xdist, 2) + Math.pow(ydist, 2));
 }
 
-function getAngle(x1, y1, x2, y2) { 
-    var xdist = x2 - x1;
-    var ydist = y2 - y1; 
-  return Math.atan2(ydist, xdist) / Math.PI * 180
+function moveTo(x1, y1, x2, y2, speed) {
+    const calc = (Math.atan2(y2 - y1, x2 - x1) / Math.PI * 180) * Math.PI / 180;
+    return {
+        x: speed * Math.cos(calc),
+        y: speed * Math.sin(calc)
+    };
 }
-
 window.addEventListener('mousemove', e => {
     mouse.x = e.clientX;
     mouse.y = e.clientY;
