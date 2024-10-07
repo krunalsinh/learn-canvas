88668c04ad8f3a95aeab5eba58798d567ab82b5e
Krunalsinh Vaghela <krunalsinh.vaghela@corpnet.co.in>
2024-10-07 17:44:33 +0530

    changes

--- 10 wave/page.js
+++ 10 wave/page.js
@@ -1,5 +1,19 @@
-import {fillRect, drawTriangle, drawCircle, drawStar} from '../common/common-functions.js';
-import {box, circle, triangle, star} from "./elements.js";
+import dat from "./dat.gui.min.js";
+// const gui = new dat.GUI();
+console.log(dat);
+
+
+// let value1 = 1;
+// const cubeFolder = gui.addFolder('Cube')
+// cubeFolder.add(value1, 'x', 0, Math.PI * 2)
+// cubeFolder.add(value1, 'y', 0, Math.PI * 2)
+// cubeFolder.add(value1, 'z', 0, Math.PI * 2)
+// cubeFolder.open()
+
+// let value2 = 1;
+// const cameraFolder = gui.addFolder('Camera')
+// cameraFolder.add(value2, 'z', 0, 10)
+// cameraFolder.open()
 
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext('2d');
@@ -14,9 +28,10 @@ const arr = [];
 ctx.beginPath();
 ctx.moveTo(0, canvas.height / 2);
 for (let i = 0; i < canvas.width; i++) {
-    ctx.lineTo(i, i);
+    ctx.lineTo(i, canvas.height / 2 + (Math.sin(i * 0.005) * 100));
 }
 ctx.lineWidth = 1;
+ctx.strokeStyle = 'red';
 ctx.stroke();
 ctx.closePath();
 // function animationFunc(){
