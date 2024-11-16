aa2c65eed8ea67c35cb4b762672fe18fa532d76c
Krunalsinh Vaghela <krunalsinh.vaghela@corpnet.co.in>
2024-11-16 18:04:40 +0530

    changes

--- 19 cross road self/elements.js
+++ 19 cross road self/elements.js
@@ -1,58 +1,75 @@
 
-import {cellSize, canvasWidth, canvasHeight} from "./page.js";
-import { drawRectangle } from "../common/common-functions.js";
+import {cellSize, canvasWidth, canvasHeight, carsImg, frogImg} from "./page.js";
+import { drawCircle, drawRectangle } from "../common/common-functions.js";
 
 class Frog{
     constructor(ctx, spriteWidth, spriteHeight){
         this.ctx = ctx;
+        this.spriteWidth = spriteWidth;
+        this.spriteHeight = spriteHeight;
+
         this.width = spriteWidth / 5;
         this.height = spriteHeight / 5;
         this.y = canvasHeight - this.height - 40;
         this.x = canvasWidth / 2 - this.width / 2;
         this.moving = true;
+        this.color = "green";
+        this.frameX = 0;
+        this.frameY = 0;
     }
     update(keys){
         
         if(keys[38] && this.moving === false){
-            if(this.height)
             this.y -= cellSize;
             this.moving = true;
+            this.frameY = 0;
         }
         if(keys[40] && this.moving === false){
             if(this.y < canvasHeight - this.height * 2 )
             this.y += cellSize;
             this.moving = true;
+            this.frameY = 3;
         }
         if(keys[37] && this.moving === false){
             if(this.x > -this.width){
                 this.x -= cellSize;
             }
             this.moving = true;
+            this.frameY = 2;
         }
         if(keys[39] && this.moving === false){
             if(this.x < canvasWidth + this.width){
                 this.x += cellSize;
             }
             this.moving = true;
+            this.frameY = 1;
         }
         this.draw();
     }
     draw(){
-        drawRectangle(this.ctx, this.x, this.y, this.width, this.height, "green")
+        // drawRectangle(this.ctx, this.x, this.y, this.width, this.height, this.color);
+        this.ctx.drawImage(frogImg, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, this.x - this.width / 2 , this.y - this.height / 2, this.width * 2, this.height * 2)
+
+    }
+    jump(){
+        if(this.moving === false) this.frameX = 1
+        else if(this.frameX === 1) this.frameX = 0;
     }
 }
 
 class Obstacle{
-    constructor(ctx, x, y, width, height, speed){
+    constructor(ctx, x, y, width, height, speed, type){
         this.ctx = ctx;
         this.width = width;
         this.height = height;
         this.y = y;
         this.x = x;
         this.speed = speed;
+        this.type = type;
     }
-    update(){
-        this.x += this.speed;
+    update(gameSpeed){
+        
+        this.x += this.speed ;
         if(this.speed < 0){
             if(this.x + this.width < 0 ){
                 this.x = canvasWidth + this.width
@@ -70,15 +87,51 @@ class Obstacle{
 }
 
 class Particle{
-    constructor(x, y, radius, color, speed){
-
+    constructor(ctx, x, y, radius,speed, color){
+        this.ctx = ctx;
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.opacity = 0.5;
+        this.speed = speed
     }
     update(){
+        this.x += this.speed.x;
+        this.y += this.speed.y;
+
+        this.opacity -= 0.02;
         this.draw();
     }
     draw(){
-
+        this.ctx.save();
+        this.ctx.globalAlpha = this.opacity;
+        drawCircle(this.ctx, this.x, this.y, this.radius, this.color)
+        this.ctx.restore();
     }
 }
 
-export { Frog , Obstacle}
+class Ripple{
+    constructor(ctx, x, y, radius, color){
+        this.ctx = ctx;
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.opacity = 0.7;
+    }
+    update(){
+        this.radius += 1;
+        this.opacity -= 0.02;
+        this.draw();
+    }
+    draw(){
+        this.ctx.save();
+        this.ctx.globalAlpha = this.opacity;
+        drawCircle(this.ctx, this.x, this.y, this.radius, this.color, true, 1, this.color)
+        this.ctx.restore();
+    }
+
+}
+
+export { Frog , Obstacle, Particle, Ripple}
--- 19 cross road self/page.js
+++ 19 cross road self/page.js
@@ -1,5 +1,5 @@
-import { loadImage } from "../common/common-functions.js";
-import { Frog, Obstacle } from "./elements.js";
+import { addText, loadImage } from "../common/common-functions.js";
+import { Frog, Obstacle, Particle, Ripple } from "./elements.js";
 
 const canvasWidth = 600;
 const canvasHeight = 600;
@@ -9,8 +9,8 @@ const ctx = canvas.getContext('2d');
 canvas.width = canvasWidth;
 canvas.height = canvasHeight;
 
-const keys = [], obstaclesArr = [], logsArr = [];
-let backgroundImg, backgroundImgRoad, frog, cellSize, grassImg;
+const keys = [], obstaclesArr = [], logsArr = [], particlesArr = [], rippleArr = [];
+let backgroundImg, backgroundImgRoad, frog, cellSize, grassImg, safe, score = 0, carsImg, frogImg, gameSpeed = 1, collisionCount = 0;
 
 await Promise.resolve(loadImage("./images/background.png"))
     .then(image => backgroundImg = image)
@@ -20,6 +20,12 @@ await Promise.resolve(loadImage("./images/background_lvl2.png"))
 
 await Promise.resolve(loadImage("./images/grass.png"))
     .then(image => grassImg = image)
+
+await Promise.resolve(loadImage("./images/cars.png"))
+    .then(image => carsImg = image)
+
+await Promise.resolve(loadImage("./images/frog_spritesheet.png"))
+    .then(image => frogImg = image)
 
 gameInit();
 addObstacles();
@@ -32,21 +38,29 @@ function gameInit() {
 
 }
 
-function animationFunc(now) {
+function animationFunc() {
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
     //water
-    //road
-    //frog
-    //obstacle
     ctx.drawImage(backgroundImg, 0, 0, canvasWidth, canvasHeight, 0, 0, canvasWidth, canvasHeight)
+    //ripple
+    handleRipples();
+    //road
     ctx.drawImage(backgroundImgRoad, 0, 0, canvasWidth, canvasHeight, 0, 0, canvasWidth, canvasHeight)
-    logsArr.forEach(log => log.update())
+    //logs & turtles
+    handlesLogs()
+    //frog
     frog.update(keys);
-    obstaclesArr.forEach(obstacle => obstacle.update());
-    ctx.drawImage(grassImg, 0, 0, canvasWidth, canvasHeight, 0, 0, canvasWidth, canvasHeight)
+    //obstacle
+    handleObstacles();
+    //dust particles
+    handleParticles();
+    //grass
+    ctx.drawImage(grassImg, 0, 0, canvasWidth, canvasHeight, 0, 0, canvasWidth, canvasHeight);
+
+    addText(ctx, 10, 50, "30px", "Open Sens", "#000", "Score : " + score);
+    addText(ctx, 10, 80, "30px", "Open Sens", "#000", "Game Speed : " + gameSpeed);
 
     requestAnimationFrame(animationFunc);
-
 
 }
 
@@ -88,7 +102,8 @@ function addObstacles() {
             i * 400,
             canvasHeight - cellSize * 5 - 20,
             cellSize * 2,
-            cellSize, -1.5));
+            cellSize, -1.5,
+            "log"));
     }
 
     //row 5
@@ -98,23 +113,109 @@ function addObstacles() {
             i * 250,
             canvasHeight - cellSize * 6 - 20,
             cellSize,
-            cellSize, 1));
+            cellSize, 1,
+            "turtle"));
     }
-    
 }
 
+function addParticles() {
+    for (let i = 0; i < 20; i++) {
+        const radius = Math.random() * 8 + 4;
+        const speed = {
+            x: Math.random() * 3 - 2,
+            y: Math.random() * 3 - 2
+        }
+        if (frog.y > 250) {
+            particlesArr.push(new Particle(ctx, frog.x + frog.width / 2, frog.y + frog.height / 2, radius, speed, "#a1a1a1"))
+        } else if (frog.y > 80) {
+            rippleArr.push(new Ripple(ctx, frog.x + frog.width / 2, frog.y + frog.height / 2, i * i * 0.1, "#fff"))
+        }
+    }
+}
+
+function handleRipples() {
+    rippleArr.forEach((ripple, index) => {
+        ripple.update();
+        if (ripple.opacity < 0.1) {
+            rippleArr.splice(index, 1);
+        }
+    })
+}
+
+function handlesLogs() {
+    safe = false;
+    logsArr.forEach(log => {
+        log.update(gameSpeed);
+
+        if (detectCollision(frog, log)) {
+            frog.x += log.speed;
+            safe = true;
+        }
+    })
+    if (!safe && (frog.y < 250 && frog.y > 90)) {
+        resetFrogPos();
+    }
+}
+
+function handleObstacles() {
+    obstaclesArr.forEach(obstacle => {
+        obstacle.update(gameSpeed);
+
+        if (detectCollision(frog, obstacle)) {
+            resetFrogPos();
+            collisionCount++;
+        }
+    });
+}
+
+function handleParticles() {
+    particlesArr.forEach((particle, index) => {
+        particle.update();
+        if (particle.opacity < 0.1) {
+            particlesArr.splice(index, 1);
+        }
+    })
+}
+
+function detectCollision(first, second) {
+    return !(first.x > second.x + second.width ||
+        first.x + first.width < second.x ||
+        first.y > second.y + second.height ||
+        first.y + first.height < second.y
+    )
+}
+
+function scored() {
+    score++;
+    gameSpeed += 0.05;
+    resetFrogPos();
+}
+
+function resetFrogPos() {
+    frog.y = canvasHeight - 90;;
+}
 //events
 addEventListener('keydown', e => {
     keys[e.keyCode] = true;
-    frog.moving = false;
 
-    // if(frog.moving === false && frog.y )
+    if (keys[38] || keys[37] || keys[40] || keys[39]) {
+        if (frog.moving === false) {
+            addParticles();
+        }
+        frog.jump();
+    }
 })
 
 addEventListener('keyup', e => {
     delete keys[e.keyCode];
+
+    if (frog.y < 0) {
+        scored();
+    }
+    frog.moving = false;
+    frog.frameX = 0;
 })
 
-export { cellSize, canvasWidth, canvasHeight }
+export { cellSize, canvasWidth, canvasHeight, carsImg, frogImg }
 
 
