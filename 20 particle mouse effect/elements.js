2bf1ed4709f93bf5ca5099c5e17db92e9bdb9372
Krunalsinh Vaghela <krunalsinh.vaghela@corpnet.co.in>
2024-11-19 18:01:51 +0530

    chnages

--- 20 particle mouse effect/elements.js
+++ 20 particle mouse effect/elements.js
@@ -3,12 +3,47 @@ import { drawCircle } from "../common/common-functions.js";
 class Particle{
     constructor(ctx, x, y, size, color){
         this.ctx = ctx;
+        this.baseX = x;
+        this.baseY = y;
         this.x = x;
         this.y = y;
         this.size = size;
         this.color = color;
+        this.dencity = Math.random() * 30 + 1;
     }
-    update(){
+    update(mouse){
+        const dx = mouse.x - this.x;
+        const dy = mouse.y - this.y;
+
+        const dist = Math.sqrt(dx * dx + dy * dy);
+
+        let forceDirX = dx / dist;
+        let forceDirY = dy / dist;
+        let maxDist = mouse.radius;
+
+        let force = (maxDist - dist) / maxDist
+
+        // console.log(forceDirX, forceDirY);
+        let dirX = forceDirX * force * this.dencity; 
+        let dirY = forceDirY * force * this.dencity; 
+        // console.log(force);
+
+        if(dist < maxDist){
+            this.x += dirX;
+            this.y += dirY;
+        }else{
+            
+            if(this.x !== this.baseX){
+                let dx = this.x - this.baseX;
+                this.x -= dx / 10;
+            }
+
+            if(this.y !== this.baseY){
+                let dy = this.y - this.baseY;
+                this.y -= dy / 10;
+            }
+        }
+
         this.draw();
     }
     draw(){
--- 20 particle mouse effect/page.js
+++ 20 particle mouse effect/page.js
@@ -1,3 +1,4 @@
+import { addText, getDistance } from "../common/common-functions.js";
 import { Particle } from "./elements.js";
 
 const canvasWidth = innerWidth;
@@ -11,6 +12,8 @@ const mouse = {
     y : null,
     radius : 150
 }
+let particle = new Particle(ctx, innerWidth / 2, innerHeight / 2, 5, "#888");
+let textData;
 
 canvas.width = canvasWidth;
 canvas.height = canvasHeight;
@@ -20,22 +23,35 @@ animationFunc();
 
 // functions
 function gameInit() {
-    for (let i = 0; i < 500; i++) {
-        const x = Math.random() * canvas.width;
-        const y = Math.random() * canvas.height;
+    addText(ctx, 10, 30, "30px", "Open Sans", "#fff","A");
 
-        particlesArr.push(new Particle(ctx, x, y, 3, "red"));
-        
+    textData = ctx.getImageData(0, 0, 100, 100);
+    // console.log(textData);
+    // for (let i = 0; i < 800; i++) {
+    //     const x = Math.random() * canvas.width;
+    //     const y = Math.random() * canvas.height;
+    //     particlesArr.push(new Particle(ctx, x, y, 2, "red"));
+    // }
+
+    for (let y = 0; y < textData.height; y++) {
+        for (let x = 0; x < textData.width; x++) {
+            if(textData.data[(y * 4 * textData.width) + (x * 4) + 3] > 128){
+                let positionX = x;
+                let positionY = y;
+                particlesArr.push(new Particle(ctx, positionX * 10, positionY * 10, 2, "white" ))
+            }
+        }
     }
+    // console.log(particlesArr);
+    
 }
 
 function animationFunc() {
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
-    particlesArr.forEach(particle => particle.update() )
+    particlesArr.forEach(particle => particle.update(mouse) )
     
     requestAnimationFrame(animationFunc);
-
 }
 
 //event
