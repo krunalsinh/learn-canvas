906fa79e60156cbd07d204f4093cd49e28136b16
Krunalsinh Vaghela <krunalsinh.vaghela@corpnet.co.in>
2024-11-26 17:56:09 +0530

    bubble text

---
+++ 23 particle mouse effect bubble/elements.js
@@ -0,0 +1,89 @@
+import { drawCircle } from "../common/common-functions.js";
+
+class Particle {
+    constructor(ctx, x, y, size, color, initX, initY) {
+        this.ctx = ctx;
+        this.baseX = x;
+        this.baseY = y;
+        this.x = initX;
+        this.y = initY;
+        this.initX = initX;
+        this.initY = initY;
+        this.size = size;
+        this.color = color;
+        this.dencity = Math.random() * 30 + 1;
+        this.initAnim = true;
+    }
+    update(mouse) {
+
+        const dx = mouse.x - this.x;
+        const dy = mouse.y - this.y;
+
+        const dist = Math.sqrt(dx * dx + dy * dy);
+
+        let forceDirX = dx / dist;
+        let forceDirY = dy / dist;
+        let maxDist = mouse.radius;
+
+        let force = (maxDist - dist) / maxDist
+
+        let dirX = forceDirX * force * this.dencity;
+        let dirY = forceDirY * force * this.dencity;
+
+        if (!this.initAnim) {
+
+            if (dist < maxDist) {
+                this.x -= dirX;
+                this.y -= dirY;
+                this.size = 15;
+            } else {
+                this.size = 7;
+
+                if (this.x !== this.baseX) {
+                    let dx = this.x - this.baseX;
+                    this.x -= dx / 10;
+                }
+
+                if (this.y !== this.baseY) {
+                    let dy = this.y - this.baseY;
+                    this.y -= dy / 10;
+                }
+            }
+
+        } else {
+            if (this.baseX !== this.initX) {
+                let dx = this.x - this.baseX;
+                this.x -= dx / 10;
+            }
+
+            if (this.baseY !== this.initY) {
+                let dy = this.y - this.baseY;
+                this.y -= dy / 10;
+            }
+            if (
+                (Math.floor(this.baseX) - Math.floor(this.x) <= 1 &&
+                    Math.floor(this.baseX) - Math.floor(this.x) >= -1) &&
+                (Math.floor(this.baseY) - Math.floor(this.y) <= 1 &&
+                    Math.floor(this.baseY) - Math.floor(this.y) >= -1)
+            ) {
+                this.initAnim = false;
+            }
+        }
+
+
+        this.draw(dist < maxDist);
+    }
+    draw(checkMaxDist) {
+        if(checkMaxDist){
+
+            drawCircle(this.ctx, this.x, this.y, this.size, this.color, true);
+            drawCircle(this.ctx, this.x - 5, this.y - 5, this.size / 4, this.color);
+            drawCircle(this.ctx, this.x + 4, this.y - 5, this.size / 7, this.color);
+        }else{
+            drawCircle(this.ctx, this.x, this.y, this.size, this.color, true);
+            drawCircle(this.ctx, this.x , this.y , this.size / 5, this.color);
+        }
+    }
+}
+
+export { Particle }
---
+++ 23 particle mouse effect bubble/page.js
@@ -0,0 +1,84 @@
+import { addText, getDistance } from "../common/common-functions.js";
+import { Particle } from "./elements.js";
+
+const canvasWidth = innerWidth;
+const canvasHeight = innerHeight;
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
+let particlesArr = [];
+const txtInput = document.getElementById("txtInput");
+const mouse = {
+    x : null,
+    y : null,
+    radius : 150
+}
+let textData, colrDeg = 0, colrDegIncr = 0, startX =0, startY = 0, endX = 0, endY = 0, offsetX = 0, offsetY = 0;
+
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+
+gameInit();
+animationFunc();
+
+// functions
+function gameInit() {
+    colrDeg = 0;
+    colrDegIncr = 0;
+    txtInput.setAttribute("disabled", "true");
+    particlesArr = [];
+    addText(ctx, 10, 30, "30px", "Open Sans", "#fff",txtInput.value ? txtInput.value : "Text");
+    
+    textData = ctx.getImageData(10, 0, txtInput.value ? txtInput.value.length * (14 + ((txtInput.value.length - 1) * 1)) 
+    : 4 * (14 + 3), 32);
+    endX = (textData.width - 1) * 10;
+    endY = (textData.height - 1) * 10;
+    offsetX = (innerWidth / 2) - (endX );
+    offsetY = (innerHeight / 2) - (endY );
+    colrDegIncr = 360 / (textData.height * textData.width);
+
+    for (let y = 0; y < textData.height; y++) {
+        for (let x = 0; x < textData.width; x++) {
+            colrDeg += colrDegIncr;
+            if(textData.data[(y * 4 * textData.width) + (x * 4) + 3] > 128){
+                let positionX = x;
+                let positionY = y;
+                let randX1 = Math.random() * -200;
+                let randX2 = Math.random() * 200 + innerWidth;
+                let randY1 = Math.random() * -200;
+                let randY2 = Math.random() * 200 + innerHeight;
+                let initX = Math.random() > 0.5 ? randX1 : randX2;
+                let initY = Math.random() > 0.5 ? randY1 : randY2;
+                particlesArr.push(new Particle(ctx, offsetX + positionX * 20, offsetY + positionY * 20, 7, `hsl(${colrDeg}, 60%, 60%)`, initX, initY ))
+                
+            }
+            
+        }
+    }
+    
+    txtInput.removeAttribute("disabled");
+    setTimeout(() => {
+
+        txtInput.focus();
+    }, 800)
+
+}
+
+function animationFunc() {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    particlesArr.forEach(particle => particle.update(mouse) )
+    
+    requestAnimationFrame(animationFunc);
+}
+
+//event
+addEventListener("mousemove", e => {
+    mouse.x = e.clientX;
+    mouse.y = e.clientY;
+})
+
+txtInput.addEventListener('input', e => {
+    gameInit()
+})
+
