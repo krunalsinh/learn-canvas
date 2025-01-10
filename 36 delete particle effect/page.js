cab7b9855c15fc0e48aaa2a708c7da51176f65c3
Krunalsinh Vaghela <krunalsinh.vaghela@corpnet.co.in>
2025-01-10 17:47:25 +0530

    changes

--- 36 delete particle effect/page.js
+++ 36 delete particle effect/page.js
@@ -1,48 +1,52 @@
+import { drawRectangle } from "../common/common-functions.js";
+
 var canvas = document.getElementById('canvas');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 var card = document.getElementById('appCard1');
+var cardBtn = document.getElementById('appCard1Btn');
 var ctx = canvas.getContext('2d');
 
 
-
-drawHTMLOnCanvas(card, ctx, 320, 180, "style1");
+appCard1Btn.addEventListener('click', function () {
+  drawHTMLOnCanvas(card, ctx, 320, 180, "style1");
+});
 
 function drawHTMLOnCanvas(element, ctx, width, height, styleId) {
-console.log(element.getBoundingClientRect());
-const eleData = element.getBoundingClientRect();
-const xPos = eleData.x;
-const yPos = eleData.y;
-console.log(xPos, yPos);    
+  // console.log(element.getBoundingClientRect());
+  const eleData = element.getBoundingClientRect();
+  const xPos = eleData.x;
+  const yPos = eleData.y;
+  // console.log(xPos, yPos);    
 
-    
-    const html = element.outerHTML;
 
-    // Get all styles from the document's stylesheets
-    let styles = "";
-    if(styleId){
-        const sheet = document.getElementById(styleId);
-        try { // Handle cross-origin stylesheets
-          for (const rule of sheet.sheet.cssRules) {
-            styles += rule.cssText;
-          }
-        } catch (e) {
-          console.warn("Cannot access cross-origin stylesheet:", sheet.href);
-        }
-    }else{
-        for (const sheet of document.styleSheets) {
-          try { // Handle cross-origin stylesheets
-            for (const rule of sheet.cssRules) {
-              styles += rule.cssText;
-            }
-          } catch (e) {
-            console.warn("Cannot access cross-origin stylesheet:", sheet.href);
-          }
-        }
+  const html = element.outerHTML;
+
+  // Get all styles from the document's stylesheets
+  let styles = "";
+  if (styleId) {
+    const sheet = document.getElementById(styleId);
+    try { // Handle cross-origin stylesheets
+      for (const rule of sheet.sheet.cssRules) {
+        styles += rule.cssText;
+      }
+    } catch (e) {
+      console.warn("Cannot access cross-origin stylesheet:", sheet.href);
     }
-    console.log(styles);
-    
-    const svg = `
+  } else {
+    for (const sheet of document.styleSheets) {
+      try { // Handle cross-origin stylesheets
+        for (const rule of sheet.cssRules) {
+          styles += rule.cssText;
+        }
+      } catch (e) {
+        console.warn("Cannot access cross-origin stylesheet:", sheet.href);
+      }
+    }
+  }
+  // console.log(styles);
+
+  const svg = `
       <svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}">
         <style>
           ${styles}
@@ -54,34 +58,68 @@ console.log(xPos, yPos);
         </foreignObject>
       </svg>
     `;
-  
-    const img = new Image();
-    img.onload = () => {
-      ctx.drawImage(img, xPos, yPos);
-    };
-    img.src = 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(svg);
+
+  const img = new Image();
+  img.onload = () => {
+    const cellCount = 30;
+    const xColCell = width / cellCount;
+    const yRowCell = height / cellCount;
+    let cells = [];
+    for (let yIndex = 0; yIndex < cellCount; yIndex++) {
+      for (let xIndex = 0; xIndex < cellCount; xIndex++) {
+        cells.push(new CanvasImgCell(
+          xPos + (xIndex * xColCell),
+          yPos + (yIndex * yRowCell),
+          img,
+          xColCell,
+          yRowCell,
+          xIndex,
+          yIndex));
+      }
+
+    }
+    card.style.display = 'none';
+
+    function animate() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      cells.forEach(cell => {
+        cell.update();
+        cell.draw(ctx);
+        cells = cells.filter(cell => !cell.markForDeletion);
+      });
+      // console.log(cells.length);
+      
+      requestAnimationFrame(animate);
+    }
+
+    animate();
+   
+
+
+  };
+  img.src = 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(svg);
 }
 
 function drawSimpleHTMLOnCanvas1(element, ctx, x, y) {
-    document.body.appendChild(element)
-    applyStyles(element)
-    document.body.removeChild(element)
+  document.body.appendChild(element)
+  applyStyles(element)
+  document.body.removeChild(element)
 
-    const tempCanvas = document.createElement('canvas');
-    tempCanvas.width = element.offsetWidth;
-    tempCanvas.height = element.offsetHeight;
-    const tempCtx = tempCanvas.getContext('2d');
+  const tempCanvas = document.createElement('canvas');
+  tempCanvas.width = element.offsetWidth;
+  tempCanvas.height = element.offsetHeight;
+  const tempCtx = tempCanvas.getContext('2d');
 
-    // Important: Add the element to the DOM (even if temporarily)
-    document.body.appendChild(element);
-    tempCtx.drawImage(element, 0, 0); // Draw the element onto the temporary canvas
-    document.body.removeChild(element); // Remove it after drawing
+  // Important: Add the element to the DOM (even if temporarily)
+  document.body.appendChild(element);
+  tempCtx.drawImage(element, 0, 0); // Draw the element onto the temporary canvas
+  document.body.removeChild(element); // Remove it after drawing
 
-    ctx.drawImage(tempCanvas, x, y);
+  ctx.drawImage(tempCanvas, x, y);
 }
 
 function applyStyles(element) {
-    if (!element || !element.style) return; // Check if element or style exists
+  if (!element || !element.style) return; // Check if element or style exists
 
   const computedStyle = window.getComputedStyle(element);
   if (!computedStyle) return; // Check if computed style exists
@@ -102,16 +140,61 @@ function applyStyles(element) {
 }
 
 function getHTML(who, deep) {
-    if (!who || !who.tagName) return '';
-    var txt, ax, el = document.createElement("div");
-    el.appendChild(who.cloneNode(false));
-    txt = el.innerHTML;
-    if (deep) {
-        ax = txt.indexOf('>') + 1;
-        txt = txt.substring(0, ax) + who.innerHTML + txt.substring(ax);
+  if (!who || !who.tagName) return '';
+  var txt, ax, el = document.createElement("div");
+  el.appendChild(who.cloneNode(false));
+  txt = el.innerHTML;
+  if (deep) {
+    ax = txt.indexOf('>') + 1;
+    txt = txt.substring(0, ax) + who.innerHTML + txt.substring(ax);
+  }
+  el = null;
+  return txt;
+}
+class CanvasImgCell {
+  constructor(x, y, img, cellSizeX, cellSizeY, xIndex, yIndex) {
+    this.x = x;
+    this.y = y;
+    this.img = img;
+    this.dx = -Math.random() * 8 + 4;
+    this.dy = -Math.random() * 8 - 4;
+    this.alpha = 1;
+    this.cellSizeX = cellSizeX;
+    this.cellSizeY = cellSizeY;
+    this.yIndex = yIndex;
+    this.xIndex = xIndex;
+    this.randomOpDecSpeed = Math.random() * 0.05 + 0.01;
+    this.markForDeletion = false;
+  }
+
+  update() {
+    this.dx *= 1.01;
+    this.dy *= 1.01;
+    if (this.alpha >= 0.10) {
+      this.alpha -= this.randomOpDecSpeed;
+    }else{
+      this.alpha = 0;
+      this.markForDeletion = true;
     }
-    el = null;
-    return txt;
+    this.x += this.dx;
+    this.y += this.dy;
+  }
+
+  draw(ctx) {
+    ctx.save();
+    ctx.globalAlpha = this.alpha;
+    ctx.drawImage(this.img,
+      this.xIndex * this.cellSizeX,
+      this.yIndex * this.cellSizeY,
+      this.cellSizeX,
+      this.cellSizeY,
+      this.x,
+      this.y,
+      this.cellSizeX,
+      this.cellSizeY
+    );
+    ctx.restore();
+  }
 }
 
 
